c2f870912c46bc70070a1630fe3a6eeafc2e5c0c
MyIFdrive <143335820+MyIFdrive@users.noreply.github.com>
2024-08-30 21:07:39 -0300

    Update button.js

--- Interações/button.js
+++ Interações/button.js
@@ -3,3 +3,29 @@ function menuOnClick() {
     document.getElementById("nav").classList.toggle("change");
     document.getElementById("menu-bg").classList.toggle("change-bg");
   }
+
+function changeMarcado() {
+  const orginal = document.querySelector('.normal').style.display='none'
+  const marcado = document.querySelector('.marcado').style.display='block'
+}
+
+function changeNormal() {
+  const orginal = document.querySelector('.normal').style.display='block'
+  const marcado = document.querySelector('.marcado').style.display='none'
+}
+
+function rotate () {
+  document.querySelector('#topo').style.rotate='405deg'
+  
+}
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+     })
+}
+
+  
+
+        
